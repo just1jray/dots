0255ad3aae8eb2b9a2eff5d404ba9992726d5636
Jesse McKnight <just1jray@gmail.com>
2022-03-29 15:44:47 -0700

    Changed file path and updated aliases

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
-grep "dotfiles/alias" ~/.zshrc &> /dev/null || echo 'source $HOME/Developer/dotfiles/alias' >> ~/.zshrc
-grep "dotfiles/function" ~/.zshrc &> /dev/null || echo 'source $HOME/Developer/dotfiles/function' >> ~/.zshrc
-grep "dotfiles/alias_$OS" ~/.zshrc &> /dev/null || echo 'source $HOME/Developer/dotfiles/alias_$OS' >> ~/.zshrc
-grep "dotfiles/function_$OS" ~/.zshrc &> /dev/null || echo 'source $HOME/Developer/dotfiles/function_$OS' >> ~/.zshrc
-grep "dotfiles/runtime" ~/.zshrc &> /dev/null || echo 'source $HOME/Developer/dotfiles/runtime' >> ~/.zshrc
+grep "dots/alias" ~/.zshrc &> /dev/null || echo 'source $HOME/Developer/src/dots/alias' >> ~/.zshrc
+grep "dots/function" ~/.zshrc &> /dev/null || echo 'source $HOME/Developer/src/dots/function' >> ~/.zshrc
+grep "dots/alias_$OS" ~/.zshrc &> /dev/null || echo 'source $HOME/Developer/src/dots/alias_$OS' >> ~/.zshrc
+grep "dots/function_$OS" ~/.zshrc &> /dev/null || echo 'source $HOME/Developer/src/dots/function_$OS' >> ~/.zshrc
+grep "dots/runtime" ~/.zshrc &> /dev/null || echo 'source $HOME/Developer/src/dots/runtime' >> ~/.zshrc
 
 source ~/.zshrc
 
