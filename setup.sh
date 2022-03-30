2e148d9d23cb2b69aed142c720f3489c8b4ff31f
just1jray <24859765+just1jray@users.noreply.github.com>
2022-03-29 22:41:03 -0700

    Added OS checks to setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+[[ `uname` == "Linux" ]] && OS=$(uname)
+[[ `uname` == "Darwin" ]] && OS="OSX"
+
 grep "dots/alias" ~/.zshrc &> /dev/null || echo "source $HOME/Developer/src/dots/alias" >> ~/.zshrc
 grep "dots/function" ~/.zshrc &> /dev/null || echo "source $HOME/Developer/src/dots/function" >> ~/.zshrc
 grep "dots/alias_$OS" ~/.zshrc &> /dev/null || echo "source $HOME/Developer/src/dots/alias_$OS" >> ~/.zshrc
