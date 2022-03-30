cfd7a37871f718650f4bffad4193ec87536263b8
Jesse McKnight <just1jray@gmail.com>
2022-03-29 17:12:58 -0700

    Changed single quotes to double quotes

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
-grep "dots/alias" ~/.zshrc &> /dev/null || echo 'source $HOME/Developer/src/dots/alias' >> ~/.zshrc
-grep "dots/function" ~/.zshrc &> /dev/null || echo 'source $HOME/Developer/src/dots/function' >> ~/.zshrc
-grep "dots/alias_$OS" ~/.zshrc &> /dev/null || echo 'source $HOME/Developer/src/dots/alias_$OS' >> ~/.zshrc
-grep "dots/function_$OS" ~/.zshrc &> /dev/null || echo 'source $HOME/Developer/src/dots/function_$OS' >> ~/.zshrc
-grep "dots/runtime" ~/.zshrc &> /dev/null || echo 'source $HOME/Developer/src/dots/runtime' >> ~/.zshrc
+grep "dots/alias" ~/.zshrc &> /dev/null || echo "source $HOME/Developer/src/dots/alias" >> ~/.zshrc
+grep "dots/function" ~/.zshrc &> /dev/null || echo "source $HOME/Developer/src/dots/function" >> ~/.zshrc
+grep "dots/alias_$OS" ~/.zshrc &> /dev/null || echo "source $HOME/Developer/src/dots/alias_$OS" >> ~/.zshrc
+grep "dots/function_$OS" ~/.zshrc &> /dev/null || echo "source $HOME/Developer/src/dots/function_$OS" >> ~/.zshrc
+grep "dots/runtime" ~/.zshrc &> /dev/null || echo "source $HOME/Developer/src/dots/runtime" >> ~/.zshrc
 
 source ~/.zshrc
 
